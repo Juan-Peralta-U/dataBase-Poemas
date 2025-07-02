575b125dc898e60c818b8e4a87a79699304fd916
saritxm <161395185+saritxm@users.noreply.github.com>
2025-07-01 20:46:28 -0500

    Create controladorObras.h

---
+++ controladorObras.h
@@ -0,0 +1,24 @@
+#ifndef CONTROLADOR_OBRAS_H
+#define CONTROLADOR_OBRAS_H
+
+#include "lista.h"
+#include "treeRB.h"
+#include "estructuras.h"
+
+class ControladorObras {
+private:
+    Lista<ObraPoetica> listaOrdenadaPorAnio;
+    TreeRB<50, ObraPoetica> arbolPorIDAutor;
+
+public:
+    ControladorObras() {}
+
+    // Métodos base que puedes implementar luego
+    void insertarObra(const ObraPoetica& obra);
+    void listarObrasPorAnio();
+    void buscarObrasPorAutor(int idAutor);
+
+    // Otros métodos de modificación, eliminación, etc.
+};
+
+#endif
