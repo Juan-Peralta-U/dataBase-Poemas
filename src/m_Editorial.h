59d5619eee86535d0fe1495d7821f18115f57de3
saritxm <161395185+saritxm@users.noreply.github.com>
2025-07-06 20:17:26 -0500

    Update m_Editorial.h

--- src/m_Editorial.h
+++ src/m_Editorial.h
@@ -1,7 +1,8 @@
 #pragma once
 #include <string>
-
+// Incluye la biblioteca estándar <string> para poder usar el tipo std::string
+// Estructura que representa una editorial en el sistema
 struct datosEditorial {
-  unsigned int IDEDITORIAL;
+  unsigned int IDEDITORIAL; // Identificador único de la editorial
   std::string nombreEditorial, ciudadOficina, paisOficina;
 };
