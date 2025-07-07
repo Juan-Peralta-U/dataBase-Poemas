b1b907527b6e2bbdc835e0ac9569dcb69e3f590e
saritxm <161395185+saritxm@users.noreply.github.com>
2025-07-06 20:20:52 -0500

    Update m_datosAutor.h

--- src/m_datosAutor.h
+++ src/m_datosAutor.h
@@ -1,4 +1,4 @@
-#pragma once
+#pragma once // Directiva que evita que el archivo se incluya más de una vez
 #include <string>
 
 /*
@@ -16,7 +16,8 @@ Datos del autor.
     • Año de inicio en la literatura • Año
 de publicación de su primera obra
 */
-
+ // Enumeración que representa la formación académica del autor.
+// Se asignan valores enteros a cada tipo de formación para facilitar almacenamiento y comparación.
 enum Formacion {
   literatura = 0,
   artes = 1,
@@ -26,14 +27,16 @@ enum Formacion {
   jurisprudencia = 5,
   otros = 6
 };
-
+// Estructura que almacena todos los datos relevantes de un autor.
+// Se utilizará en el sistema para gestionar información biográfica y profesional.
 struct datosAutor {
-  unsigned int IDAUTOR;
-  bool sexo;
+  unsigned int IDAUTOR; // Identificador único del autor
+  bool sexo; // Representa el sexo: false = Femenino, true = Masculino
   int añoIncio, añoPublicacion;
   int fechaNacimiento[3];  // tres ints para dia/mes/año facilita calculos
                            // espero
   Formacion formacion;
+ // Datos personales y geográficos del autor
   std::string nombre, Apellido, cidudadNacimiento, paisNacimiento,
       ciudadResidencia;
 };
