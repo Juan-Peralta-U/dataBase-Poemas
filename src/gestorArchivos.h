db3bad24e82c04f8f65e99d7efca9c4290789507
saritxm <161395185+saritxm@users.noreply.github.com>
2025-07-06 21:30:01 -0500

    Update gestorArchivos.h

--- src/gestorArchivos.h
+++ src/gestorArchivos.h
@@ -1,31 +1,41 @@
 #ifndef GESTION_DATOS_H
 #define GESTION_DATOS_H
-
+// Inclusión de bibliotecas necesarias para el uso de std::string
 #include <string>
-
+// Inclusión de controladores para acceder y modificar las estructuras de datos principales
 #include "controladorAutores.h"
 #include "controladorEditoriales.h"
 #include "controladorObras.h"
-
-// Funciones para cargar datos
+// ----------------------
+// FUNCIONES DE CARGA
+// ----------------------
+// Carga los autores desde un archivo CSV (texto separado por comas) al sistema
 void cargarAutores(ControladorAutores& ctrlAutores,
                    const std::string& nombreArchivo);
+// Carga las obras desde un archivo CSV y las enlaza con los autores
 void cargarObras(ControladorObras& ctrlObras, ControladorAutores& ctrlAutores,
                  const std::string& nombreArchivo);
+// Carga las editoriales desde un archivo CSV al sistema
 void cargarEditoriales(ControladorEditoriales& ctrlEditoriales,
                        const std::string& nombreArchivo);
-
+// Carga las ediciones desde un archivo CSV y las enlaza con obras y editoriales
 void cargarEdiciones(ControladorObras& ctrlObras,
                      ControladorEditoriales& ctrlEditoriales,
                      const string& nombreArchivo);
+// ----------------------
+// FUNCIONES DE GUARDADO
+// ----------------------
 
-// Funciones para guardar datos
+// Guarda los autores registrados en el sistema dentro de un archivo CSV
 void guardarAutores(ControladorAutores& ctrlAutores,
                     const std::string& nombreArchivo);
+// Guarda las editoriales registradas en el sistema dentro de un archivo CSV
 void guardarEditoriales(ControladorEditoriales& ctrlEditoriales,
                         const std::string& nombreArchivo);
+// Guarda todas las ediciones de obras dentro de un archivo CSV
 void guardarEdiciones(ControladorObras& ctrlObras,
                       const std::string& nombreArchivo);
+// Guarda todas las obras literarias del sistema en un archivo CSV
 void guardarObras(ControladorObras& ctrlObras,
                   const std::string& nombreArchivo);
 
